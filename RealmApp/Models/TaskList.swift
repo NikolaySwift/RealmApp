8200156e493aca1228284e24ccb1d4f4f5488e10
NikolayD <volodar86@yandex.ru>
2024-09-01 11:20:50 +0300

    code from lesson

---
+++ RealmApp/Models/TaskList.swift
@@ -0,0 +1,22 @@
+//
+//  TaskList.swift
+//  RealmApp
+//
+//  Created by NikolayD on 30.08.2024.
+//
+
+import Foundation
+import RealmSwift
+
+final class TaskList: Object {
+    @Persisted var title = ""
+    @Persisted var date = Date()
+    @Persisted var tasks = List<Task>()
+}
+
+final class Task: Object {
+    @Persisted var title = ""
+    @Persisted var note = ""
+    @Persisted var date = Date()
+    @Persisted var isComplete = false
+}
